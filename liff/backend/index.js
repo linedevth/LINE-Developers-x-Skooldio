58a936286ec8422413d1360091596746205a1da0
Jirawat Karanwittayakarn <jirawatee@gmail.com>
2020-03-05 10:57:54 +0700

    Add files via upload

--- liff/backend/index.js
+++ liff/backend/index.js
@@ -1,3 +1,5 @@
+const request = require('request-promise')
+
 // Verifying Access Token and Channel ID
 const json = await request.get({
   url: `https://api.line.me/oauth2/v2.1/verify?access_token=${accessToken}`,
@@ -9,7 +11,7 @@ if (json.client_id !== CHANNEL_ID) {
 
 
 // Getting User Profile by Access Token
-return await request.get({
+const profile = await request.get({
   url: "https://api.line.me/v2/profile",
   headers: { Authorization: `Bearer ${accessToken}` },
   json: true
@@ -18,11 +20,11 @@ return await request.get({
 
 // Revoke Access Token
 await request.post({
-  url: "https://api.line.me/oauth2/v2.1/revoke",
-  headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  form: {
-    access_token: `${accessToken}`,
-    client_id: CHANNEL_ID,
-    client_secret: CHANNEL_SECRET
-  }
+ url: "https://api.line.me/oauth2/v2.1/revoke",
+ headers: { "Content-Type": "application/x-www-form-urlencoded" },
+ form: {
+   access_token: `${accessToken}`,
+   client_id: CHANNEL_ID,
+   client_secret: CHANNEL_SECRET
+ } 
 })
